a44708b656b4575c7a0ebc6b2b9ab81c534e8ca2
DavidCano <d.cano.rosillo@gmail.com>
2023-02-24 23:46:59 +0100

    falta infer_predict

--- inference.py
+++ inference.py
@@ -1,20 +1,34 @@
+import torch
 from PIL import Image
+from lavis.models import load_model_and_preprocess
 
+device=torch.device("cpu")
 cap_model, cap_processor = None, None
 clip_model, clip_text_processor, clip_image_processor = None, None, None
 predict_type_model, predict_type_processor = None, None
 
 def infer_generate_caption(img:Image):
-    global cap_model, cap_processor
+    global cap_model, cap_processor, device
     if cap_model == None:
-        cap_model, cap_processor = None, None
+        cap_model, cap_processor, _ = load_model_and_preprocess(name="blip_caption", model_type="base_coco", is_eval=True, device=device)
+    img = cap_processor["eval"](img).unsqueeze(0).to(device)
+    caption = cap_model.generate({"image": img})
+    return caption
 
 def infer_compute_similarity(text:str, img:Image):
-    global clip_model, clip_text_processor, clip_image_processor
+    global clip_model, clip_image_processor, clip_text_processor, device
     if clip_model == None:
-        clip_model, clip_text_processor, clip_image_processor = None, None, None
+        clip_model, clip_image_processor, clip_text_processor = load_model_and_preprocess(name="blip_feature_extractor", model_type="base", is_eval=True, device=device)
+    image = clip_image_processor["eval"](img).unsqueeze(0).to(device)
+    text_input = clip_text_processor["eval"](text)
+    sample = {"image": image, "text_input": [text_input]}
+    features_image = clip_model.extract_features(sample, mode="image")
+    features_text = clip_model.extract_features(sample, mode="text")
+    similarity = features_image.image_embeds_proj[:, 0, :] @ features_text.text_embeds_proj[:, 0, :].t()
+    return similarity.item()
+
 
 def infer_predict_type(img:Image):
-    global predict_type_model, predict_type_processor
+    global predict_type_model, predict_type_processor, device
     if predict_type_model == None:
         predict_type_model, predict_type_processor = None, None
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
 from PIL import Image
 from io import BytesIO
+from inference import infer_generate_caption, infer_compute_similarity, infer_predict_type
 import uvicorn
 
 app = FastAPI(title='Pautas E2R imagenes')
@@ -10,23 +11,26 @@ async def generate_caption(file: UploadFile = File(...)):
     extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
     if not extension:
         return "Image must be jpg or png format!"
-    image = Image.open(BytesIO(await file.read()))
-    return 'hi'
+    image = Image.open(BytesIO(await file.read())).convert("RGB")
+    caption = infer_generate_caption(image)
+    return caption
 
 @app.post('/compute_similarity')
 async def compute_similarity(text:str, file: UploadFile = File(...)):
     extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
     if not extension:
         return "Image must be jpg or png format!"
-    image = Image.open(BytesIO(await file.read()))
-    return 'hi'
+    image = Image.open(BytesIO(await file.read())).convert("RGB")
+    similarity = infer_compute_similarity(text, image)
+    print(similarity)
+    return similarity
 
 @app.post('/predict_type')
 async def predict_type(file: UploadFile = File(...)):
     extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
     if not extension:
         return "Image must be jpg or png format!"
-    image = Image.open(BytesIO(await file.read()))
+    image = Image.open(BytesIO(await file.read())).convert("RGB")
     return 'hi'
 
 if __name__ == "__main__":
