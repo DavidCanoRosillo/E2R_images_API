0281074ec3a7ee1efe8216219d910684a5467acc
DavidCano <d.cano.rosillo@gmail.com>
2023-03-07 21:23:39 +0100

    Funcionalidad hecha falta css

--- frontend/js/test.js
+++ frontend/js/test.js
@@ -2,7 +2,7 @@ let imageFile = null;
 
 async function generateCaption() {
   if (!imageFile){
-    alert("Please insert image");
+    alert("Porfavor inserte imagen");
     return;
   }
 
@@ -14,16 +14,15 @@ async function generateCaption() {
     body: formData
   });
 
-  const result = await response.text();
-
+  const result = await response.json();
   var element = document.getElementById("caption_result");
-   element.innerHTML = result;
+  element.innerHTML = result;
 }
 
 
 async function classifyImage() {
   if (!imageFile){
-    alert("Please insert image");
+    alert("Porfavor inserte imagen");
     return;
   }
 
@@ -35,18 +34,31 @@ async function classifyImage() {
     body: formData
   });
 
-  const result = await response.text();
-
-  var element = document.getElementById("caption_result");
-   element.innerHTML = result;
+  const result = await response.json();
+  
+  const ul = document.getElementById('clases');
+  const listItems = ul.getElementsByTagName('li');
+  
+  listItems[0].innerHTML = "Sin gráfico: " + result["just_image"] + "%";
+  listItems[1].innerHTML = "Gráfico de barras: " + result["bar_chart"] + "%";
+  listItems[2].innerHTML = "Diagrama: " + result["diagram"] + "%";
+  listItems[3].innerHTML = "Gráfico de flujo: " + result["flow_chart"] + "%";
+  listItems[4].innerHTML = "Gráfico de ejes: " + result["graph"] + "%";
+  listItems[5].innerHTML = "Gráfico de crecimiento: " + result["growth_chart"] + "%";
+  listItems[6].innerHTML = "Gráfico circular: " + result["pie_chart"] + "%";
+  listItems[7].innerHTML = "Gráfico de tabla: " + result["table"] + "%";
 }
 
 async function calculateSimilarity() {
   if (!imageFile){
-    alert("Please insert image");
+    alert("Porfavor inserte imagen");
     return;
   }
   const query_text = document.getElementById("form").elements;
+  if (!query_text[0].value){
+    alert("Porfavor inserte texto");
+    return;
+  }
   const formData = new FormData();
   formData.append('file', imageFile);
 
@@ -55,10 +67,9 @@ async function calculateSimilarity() {
     body: formData
   });
 
-  const result = await response.text();
-
-  var element = document.getElementById("caption_result");
-  element.innerHTML = result;
+  const result = await response.json();
+  var element = document.getElementById("similarity_result");
+  element.innerHTML = "Porcentaje obtenido " + result['max_score'] + "% \n" + "Texto adecuado a imagen " + result['adequate'] + ".\n Limite usado para decidirlo " + result["limit_used"] + "%.";
 }
 
 function loadImage(event){
