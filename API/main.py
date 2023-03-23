f00c48ab82bca33af700ac9ee0dade9c6e2e7ae7
David Cano Rosillo <dcr@Davids-MacBook-Pro.local>
2023-03-23 23:46:36 +0100

    with css

--- API/main.py
+++ API/main.py
@@ -1,26 +1,23 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, Request, File, UploadFile
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from PIL import Image
 from io import BytesIO
 from inference import infer_generate_caption, infer_compute_similarity, infer_predict_type
 import uvicorn
 from utils import translate
 
+
 app = FastAPI(title='Pautas E2R imagenes')
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/", response_class=HTMLResponse)
-async def read_items():
-    f = open("./frontend/index.html")
-    html_content = f.read()
-    print(html_content)
-    return HTMLResponse(content=html_content, status_code=200)
-
-@app.get("/js/test.js", response_class=HTMLResponse)
-async def read_js():
-    f = open("./frontend/js/test.js")
-    html_content = f.read()
-    return HTMLResponse(content=html_content, status_code=200)
-
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post('/generate_caption')
 async def generate_caption(file: UploadFile = File(...)):
@@ -51,6 +48,6 @@ async def predict_type(file: UploadFile = File(...)):
     image = Image.open(BytesIO(await file.read())).convert("RGB")
     predictions = infer_predict_type(image)
     return predictions
-
+    
 if __name__ == "__main__":
-    uvicorn.run(app, port='1234', host='0.0.0.0')
+    uvicorn.run(app, port='80', host='0.0.0.0')
